4e8c5f7f4773d53ae795d334c9482b49fcfeacfb
ikanx101 <ikanx101@gmail.com>
2020-11-02 08:48:42 +0700

    versi 08.48

--- LEFO Market Research/EXPSS/skrip LEFO.R
+++ LEFO Market Research/EXPSS/skrip LEFO.R
@@ -1,9 +1,11 @@
 # Materi training hari ini
 
 # TUJUAN
-#   1. membuat tabulasi frekuensi
-#   2. membuat cross tabulasi
-#   3. membuat uji signifikansi dari cross tabulasi
+  # 1. membuat tabulasi frekuensi
+  # 2. membuat cross tabulasi
+  # 3. membuat uji signifikansi dari cross tabulasi
+  # 4. melakukan recode
+  # 5. Export tabulasi ke format excel
 
 # sebelumnya, pastikan working directory kita berada di directory yang tepat
 
@@ -35,19 +37,35 @@ str(data)
   # terlihat ada 262 baris (responden)
   # dan ada 17 kolom (pertanyaan atau variabel)
 
+
+# ====================================
+# ====================================
+
 # sekarang kita akan membuat tabulasi frekuensi
 
   # tabulasi frekuensi real number
-data %>% 
+tabel_1 = 
+  data %>% 
   tab_cells(jk) %>% 
-  tab_stat_cpct() %>% 
-  tab_pivot()
+  tab_stat_cases() %>% 
+  tab_pivot() %>% 
+  set_caption("Real Number Jenis Kelamin")
+
+  # bagaimana cara melihat tabel_1?
+    # kita panggil saja
+tabel_1
 
   # tabulasi frekuensi persentase
-data %>% 
+tabel_2 = 
+  data %>% 
   tab_cells(jk) %>% 
   tab_stat_cpct() %>% 
-  tab_pivot()
+  tab_pivot() %>% 
+  set_caption("Percentage Jenis Kelamin")
+
+  # bagaimana cara melihat tabel_2?
+    # kita panggil saja
+tabel_2
 
 # gimana?
 # mudah kan?
