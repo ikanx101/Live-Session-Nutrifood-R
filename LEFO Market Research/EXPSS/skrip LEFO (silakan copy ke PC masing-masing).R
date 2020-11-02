bafb58bb3690810be7fee5d655477bf540b4ad74
mr.ikanx <ikanx101@gmail.com>
2020-11-02 00:29:39 +0000

    silakan ya

---
+++ LEFO Market Research/EXPSS/skrip LEFO (silakan copy ke PC masing-masing).R
@@ -0,0 +1,52 @@
+# Materi training hari ini
+
+# TUJUAN
+#   1. membuat tabulasi frekuensi
+#   2. membuat cross tabulasi
+#   3. membuat uji signifikansi dari cross tabulasi
+
+# sebelumnya, pastikan working directory kita berada di directory yang tepat
+
+# mari kita mulai ya
+
+# ====================================
+
+# bersih-bersih global environment
+rm(list = ls())
+
+# panggil libraries
+library(readxl)
+library(dplyr)
+library(expss)
+
+# panggil dataset latihan
+  # salah satu keuntungan mengolah data entry hasil survey dengan R adalah kita
+  # tidak perlu membuat koding jawaban text ke dalam numerik.
+  # biarkan saja data hasil entry di excel memiliki bentuk teks.
+
+data = read_excel("dataset latihan.xlsx")
+
+# cek struktur data
+  # sebelum bekerja lebih lanjut, mari kita lihat struktur data tersebut!
+
+str(data)
+
+  # terlihat ada 262 baris (responden)
+  # dan ada 17 kolom (pertanyaan atau variabel)
+
+# sekarang kita akan membuat tabulasi frekuensi
+
+  # tabulasi frekuensi real number
+data %>% 
+  tab_cells(jk) %>% 
+  tab_stat_cpct() %>% 
+  tab_pivot()
+
+  # tabulasi frekuensi persentase
+data %>% 
+  tab_cells(jk) %>% 
+  tab_stat_cpct() %>% 
+  tab_pivot()
+
+# gimana?
+# mudah kan?
