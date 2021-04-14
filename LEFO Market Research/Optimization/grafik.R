bd2b129fe6da7e23253f59c60b5f23bdef92922e
ikanx101 <ikanx101@gmail.com>
2021-04-14 14:31:00 +0700

    updates

---
+++ LEFO Market Research/Optimization/grafik.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(ggforce)
+library(reshape2)
+
+rm(list=ls())
+
+jadwal = readxl::read_excel("ppic.xlsx")
+
+jadwal %>% 
+  ggplot(aes(x = minggu, label = label, fill = minggu)) +
+  
+  annotate("segment", x = -2, xend = -1, y = 0, yend = 0, size = 1, color = "orange")+
+  annotate("segment", x = -1, xend = 0, y = 0, yend = 0, size = 1, color = "yellow")+
+  annotate("segment", x = 0, xend = 1, y = 0, yend = 0, size = 1, color = "blue")+
+  annotate("segment", x = 1, xend = 2, y = 0, yend = 0, size = 1, color = "steelblue") +
+  annotate("segment", x = 2, xend = 3, y = 0, yend = 0, size = 1, color = "black") +
+  annotate("segment", x = 3, xend = 4, y = 0, yend = 0, size = 1, color = "darkred") +
+  
+  geom_point(aes(y = 0), size = 3, shape = 21, stroke = 1.5, fill = "white") +
+  
+  geom_label(aes(y = 0, label = label_minggu),
+             fill = "black",
+             color = "white") +
+  
+  geom_mark_circle(aes(y = 0, label = NA, description = label, fill = event), 
+                   con.cap = -1, fill = NA, color = NA, label.fontsize = 10, label.hjust = 0,
+                   label.fill = NA,
+                   con.size = 0.5, con.border = "one",
+                   con.type = "elbow") +
+
+  labs(title = "Timeline Pengadaan Bahan Baku Gula",
+       subtitle = "minggu dalam suatu bulan",
+       caption = "dibuat dengan R")+
+  
+  theme_void() +
+  
+  theme(legend.position = "none",
+        plot.background = element_rect(fill = "#F9F8F1", color = NA),
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 25, family = "Patua"),
+        plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 22),
+        plot.caption = element_text(hjust = 0.5, color = "grey50", family = "Lato")) +
+  xlim(-3,5)+ ylim(-30,30)
+
+ggsave("gula.png", width = 15, height = 7)
