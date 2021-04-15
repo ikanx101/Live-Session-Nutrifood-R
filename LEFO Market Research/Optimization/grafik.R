3adf63c9c75b6fd97cd39150873856137eee5084
ikanx101 <ikanx101@gmail.com>
2021-04-15 17:11:40 +0700

    update baru

--- LEFO Market Research/Optimization/grafik.R
+++ LEFO Market Research/Optimization/grafik.R
@@ -6,19 +6,16 @@ library(reshape2)
 
 rm(list=ls())
 
-jadwal = readxl::read_excel("ppic.xlsx")
+jadwal = readxl::read_excel("ppic.xlsx", sheet = "Contoh Kasus")
 
 jadwal %>% 
   ggplot(aes(x = minggu, label = label, fill = minggu)) +
   
-  annotate("segment", x = -2, xend = -1, y = 0, yend = 0, size = 1, color = "orange")+
-  annotate("segment", x = -1, xend = 0, y = 0, yend = 0, size = 1, color = "yellow")+
   annotate("segment", x = 0, xend = 1, y = 0, yend = 0, size = 1, color = "blue")+
   annotate("segment", x = 1, xend = 2, y = 0, yend = 0, size = 1, color = "steelblue") +
   annotate("segment", x = 2, xend = 3, y = 0, yend = 0, size = 1, color = "black") +
   annotate("segment", x = 3, xend = 4, y = 0, yend = 0, size = 1, color = "darkred") +
-  annotate("segment", x = 4, xend = 5, y = 0, yend = 0, size = 1, color = "grey") +
-  
+
   geom_point(aes(y = -.1), size = 1, shape = 21, stroke = 1.5, fill = "white") +
   
   geom_label(aes(y = 0, label = label_minggu),
@@ -31,17 +28,17 @@ jadwal %>%
                    con.size = 0.5, con.border = "one",
                    con.type = "elbow") +
 
-  labs(title = "Timeline Pengadaan Bahan Baku Gula",
+  labs(title = "Ilustrasi Pemesanan Gula",
        subtitle = "Garis horizontal menandakan minggu",
        caption = "Tim Optimis Nutrifood\ndibuat dengan R")+
   
   theme_void() +
   
   theme(legend.position = "none",
-        plot.background = element_rect(fill = "#F9F8F1", color = NA),
+        plot.background = element_rect(fill = "#d9bab8", color = NA),
         plot.title = element_text(hjust = 0.5, face = "bold", size = 25, family = "Patua"),
         plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 22),
         plot.caption = element_text(hjust = 0.5, color = "grey50", family = "Lato")) +
-  xlim(-3,5)+ ylim(-30,30)
+  xlim(-1,5)+ ylim(-20,20)
 
-ggsave("gula_new.png", width = 15, height = 7)
+ggsave("ilustrasi.png", width = 15, height = 7)
