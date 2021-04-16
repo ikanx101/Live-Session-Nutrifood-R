d65b480d7961e55707cecd3ef2508014afc34fcf
ikanx101 <ikanx101@gmail.com>
2021-04-16 17:31:52 +0700

    grafik bagan baru

--- LEFO Market Research/Optimization/grafik.R
+++ LEFO Market Research/Optimization/grafik.R
@@ -6,17 +6,20 @@ library(reshape2)
 
 rm(list=ls())
 
-jadwal = readxl::read_excel("ppic.xlsx", sheet = "Contoh Kasus")
+jadwal = readxl::read_excel("ppic.xlsx", sheet = "jadwal all")
 
 jadwal %>% 
   ggplot(aes(x = minggu, label = label, fill = minggu)) +
   
-  annotate("segment", x = 0, xend = 1, y = 0, yend = 0, size = 1, color = "blue")+
-  annotate("segment", x = 1, xend = 2, y = 0, yend = 0, size = 1, color = "steelblue") +
-  annotate("segment", x = 2, xend = 3, y = 0, yend = 0, size = 1, color = "black") +
-  annotate("segment", x = 3, xend = 4, y = 0, yend = 0, size = 1, color = "darkred") +
-
-  geom_point(aes(y = -.1), size = 1, shape = 21, stroke = 1.5, fill = "white") +
+  annotate("segment", x = 0, xend = 1, y = 0, yend = 0, size = 1, color = "lightblue") +
+  annotate("segment", x = 1, xend = 2, y = 0, yend = 0, size = 1, color = "blue") +
+  annotate("segment", x = 2, xend = 3, y = 0, yend = 0, size = 1, color = "darkblue") +
+  annotate("segment", x = 3, xend = 4, y = 0, yend = 0, size = 1, color = "steelblue") +
+  annotate("segment", x = 4, xend = 5, y = 0, yend = 0, size = 1, color = "green") +
+  annotate("segment", x = 5, xend = 6, y = 0, yend = 0, size = 1, color = "black") +
+  annotate("segment", x = 6, xend = 7, y = 0, yend = 0, size = 1, color = "darkred") +
+  
+  geom_point(aes(y = -.1), size = 2, shape = 21, stroke = 1.5, fill = "white") +
   
   geom_label(aes(y = 0, label = label_minggu),
              fill = "black",
@@ -28,17 +31,17 @@ jadwal %>%
                    con.size = 0.5, con.border = "one",
                    con.type = "elbow") +
 
-  labs(title = "Ilustrasi Pemesanan Gula",
+  labs(title = "Timeline Pengadaan Bahan Baku Gula",
        subtitle = "Garis horizontal menandakan minggu",
-       caption = "Tim Optimis Nutrifood\ndibuat dengan R")+
+       caption = "Produksi sebenarnya dimulai pada saat minggu III. Namun, sejak gula mulai dikirim pada minggu I, kita harus mulai memperhitungkan kapasitas gudang.\nDemikian juga saat pengiriman gula di minggu II. Oleh karena itu pemakaian pada minggu I dan II akan dijadikan parameter dalam model matematika.\n Tim Optimis Nutrifood\ndibuat dengan R")+
   
   theme_void() +
   
   theme(legend.position = "none",
-        plot.background = element_rect(fill = "#d9bab8", color = NA),
+        plot.background = element_rect(fill = "#e1f7e7", color = NA),
         plot.title = element_text(hjust = 0.5, face = "bold", size = 25, family = "Patua"),
         plot.subtitle = element_text(hjust = 0.5, face = "italic", size = 22),
-        plot.caption = element_text(hjust = 0.5, color = "grey50", family = "Lato")) +
-  xlim(-1,5)+ ylim(-20,20)
+        plot.caption = element_text(hjust = 0.5, face = "bold", size = 10, color = "grey50", family = "Lato")) +
+  xlim(0,7)+ ylim(-30,30)
 
-ggsave("ilustrasi.png", width = 15, height = 7)
+ggsave("gula_new.png", width = 15, height = 7)
