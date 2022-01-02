699a55ecffb57ba510ef8a8b875e647f6aa2503e
ikanx101 <ikanx101@gmail.com>
2022-01-02 07:27:07 +0700

    update

--- LEFO Market Research/Meeting Rutin Tim Data Sains/Parallel/algo/pi/pi_paralel_affinity.R
+++ LEFO Market Research/Meeting Rutin Tim Data Sains/Parallel/algo/pi/pi_paralel_affinity.R
@@ -1,4 +1,5 @@
 rm(list=ls())
+set.seed(2022)
 
 # initial condition
 nx = 10^8
@@ -8,6 +9,7 @@ fx = function(x){4 * sqrt(1 - x^2)}
 
 # paralel
 hitung_pi_par = function(n){
+  xi = runif(n)
   fxi = fx(xi)
   pi_numerik = mean(fxi)
   return(pi_numerik)
@@ -17,27 +19,23 @@ hitung_pi_par = function(n){
 library(parallel)
 numCores = detectCores()
 
-# hitung waktu
-mulai = Sys.time()
-# generate
-xi = runif(nx)
-
 # kita pecah
 n_pecah = nx / numCores
-x1 = xi[1:n_pecah]
-x2 = xi[(n_pecah+1):(n_pecah*2)]
-x3 = xi[(2*n_pecah+1):(n_pecah*3)]
-x4 = xi[(3*n_pecah+1):(n_pecah*4)]
-x5 = xi[(4*n_pecah+1):(n_pecah*5)]
-x6 = xi[(5*n_pecah+1):(n_pecah*6)]
-x7 = xi[(6*n_pecah+1):(n_pecah*7)]
-x8 = xi[(7*n_pecah+1):(n_pecah*8)]
 
 # affinity
 affi = 1:8
 
+# hitung waktu
+mulai = Sys.time()
+
 print("Hasil Menggunakan Parallel Processing:")
-mclapply(list(x1,x2,x3,x4,x5,x6,x7,x8),hitung_pi_par,mc.preschedule = FALSE,affinity.list = affi)
+pi = mclapply(list(n_pecah,n_pecah,n_pecah,n_pecah,
+                   n_pecah,n_pecah,n_pecah,n_pecah),
+              hitung_pi_par,mc.preschedule = FALSE,affinity.list = affi,mc.cores = numCores)
+
+pi = mean(unlist(pi))
 
 waktu = Sys.time() - mulai
-  
+
+print(pi)
+print(waktu)  
