b198019647e4a0aaa5ed71b126ed7de857bbcbb4
ikanx101 <ikanx101@gmail.com>
2022-01-03 00:45:02 +0000

    complete

---
+++ LEFO Market Research/Meeting Rutin Tim Data Sains/Parallel/algo/tidy_furrr/regresi_furrr.R
@@ -0,0 +1,35 @@
+rm(list=ls())
+
+library(dplyr)
+library(purrr)
+library(furrr)
+library(tidyr)
+
+
+df = mtcars %>% select(mpg,am,gear) %>% nest(data = c(mpg,gear))
+
+# ini kita set agar jalan di 6 cores
+plan(multisession,workers = 7)
+
+start = Sys.time()
+
+model_df = 
+  df %>%
+  mutate(model = future_map(data, function(df){
+		     lm(mpg ~ gear, data = df)
+		     }
+		    )
+	)
+
+waktu = Sys.time() - start
+
+cat(rep("\n",20))
+print("Modelling regresi linear parallel dengan furrr")
+print("Menggunakan data mtcars, melakukan regresi linear antara:")
+print("mpg ~ gear per tipe transmisi mobil")
+print("===========================================================")
+print("Runtime:")
+print(waktu)
+
+print(model_df$model[1])
+print(model_df$model[2])
