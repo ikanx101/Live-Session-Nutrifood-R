835ce06036a579b9550fbfa02ce2a7a951581a20
ikanx101 <ikanx101@gmail.com>
2022-01-03 00:39:36 +0000

    purrr algo

---
+++ LEFO Market Research/Meeting Rutin Tim Data Sains/Parallel/algo/tidy_furrr/algo.R
@@ -0,0 +1,32 @@
+rm(list=ls())
+
+library(dplyr)
+library(purrr)
+library(furrr)
+library(tidyr)
+
+
+df = mtcars %>% select(mpg,am,gear) %>% nest(data = c(mpg,gear))
+
+start = Sys.time()
+
+model_df = 
+  df %>%
+  mutate(model = map(data, function(df){
+		     lm(mpg ~ gear, data = df)
+		     }
+		    )
+	)
+
+waktu = Sys.time() - start
+
+cat(rep("\n",20))
+print("Modelling regresi linear standar dengan purrr")
+print("Menggunakan data mtcars, melakukan regresi linear antara:")
+print("mpg ~ gear per tipe transmisi mobil")
+print("===========================================================")
+print("Runtime:")
+print(waktu)
+
+print(model_df$model[1])
+print(model_df$model[2])
