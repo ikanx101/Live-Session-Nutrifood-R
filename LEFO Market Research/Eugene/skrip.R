b7d4f84366859fab0f9c67fc7974cd5846b5d463
ikanx101 <ikanx101@gmail.com>
2022-06-18 13:47:57 +0000

    done

--- LEFO Market Research/Eugene/skrip.R
+++ LEFO Market Research/Eugene/skrip.R
@@ -21,7 +21,9 @@ for(i in 2:nrow(utama)){
   if(utama$id[i] - utama$id[i-1] == 1){utama$grup[i] = utama$grup[i-1]}
   else if(utama$id[i] - utama$id[i-1] != 1){utama$grup[i] = utama$grup[i-1] + 1}
   }
-
+# kita beberes biar nanti langsung tempel
+colnames(utama)[1:2] = c("prod_code","prod_desc")
+utama = utama %>% select(-id) %>% group_split(grup)
 
 # lihat hasilnya
 utama
@@ -30,12 +32,45 @@ utama
 lainnya = 
   df %>%
   filter(!is.na(raw_mat_3))
-
+# kita buat pengelompokan dengan cara yang sama dengan sebelumnya
 lainnya$grup = 1
 for(i in 2:nrow(lainnya)){
   if(lainnya$id[i] - lainnya$id[i-1] == 1){lainnya$grup[i] = lainnya$grup[i-1]}
   else if(lainnya$id[i] - lainnya$id[i-1] != 1){lainnya$grup[i] = lainnya$grup[i-1] + 1}
   }
+# kita beberes lagi agar nanti tinggal dimatching-kan
+colnames(lainnya)[1:2] = c("form_code","form_desc")
+lainnya = lainnya %>% select(-id) %>% group_split(grup)
 
 # lihat hasilnya
 lainnya
+
+# =======================================================
+# saatnya kita gabung
+
+# siapin rumah untuk menampungnya
+n = length(utama)
+hasil = vector("list",n)
+
+# mulai iterasinya
+for(i in 1:n){
+  # ambil data per grup
+  temp_1 = utama[[i]] %>% select(-grup)
+  temp_2 = lainnya[[i]] %>% select(-grup)
+  
+  # hitung baris
+  n1 = nrow(temp_1)
+  n2 = nrow(temp_2)
+  # jika barisnya kurang, maka diisi dulu
+  if(n1 < n2){
+   for(j in (n1+1):n2){
+    temp_1[j,] = list(NA,NA)
+    }
+  }
+  # gabung
+  hasil[[i]] =  cbind(temp_1,temp_2)
+}
+
+final = do.call(rbind,hasil)
+
+final
