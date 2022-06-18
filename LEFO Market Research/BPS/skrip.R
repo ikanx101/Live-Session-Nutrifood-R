aadc48726783bab4c9fba80445c4e24354f340ac
ikanx101 <ikanx101@gmail.com>
2022-06-18 21:07:38 +0000

    abracadabra

--- LEFO Market Research/BPS/skrip.R
+++ LEFO Market Research/BPS/skrip.R
@@ -3,8 +3,10 @@ rm(list=ls())
 library(readxl)
 library(dplyr)
 
+# data mentah
 df = read_excel("Rekap Sensus BPS 2020 (5).xlsx",sheet = "Raw BPS",skip = 4)
 
+# hitung total sensus dari data mentah
 df %>% 
   filter(grepl("total",gender,ignore.case = T)) %>% 
   filter(grepl("total",usia,ignore.case = T)) %>% 
@@ -12,6 +14,7 @@ df %>%
   mutate(jml_penduduk = as.numeric(jml_penduduk)) %>% 
   summarise(sum(jml_penduduk))
 
-
+# ambil data clean
 df_clean = read_excel("Rekap Sensus BPS 2020 (5).xlsx",sheet = "Clean BPS",skip = 4)
+# ambil total data clean
 df_clean %>% summarise(sum(total_penduduk))
