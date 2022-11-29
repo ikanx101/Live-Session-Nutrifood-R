68d1986f7bba6f5f235097e2c8ade994ddc7b2c7
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-11-29 17:46:43 +0700

    Update scraper.R

--- Kaggle Data/WC Qatar 2022/scraper.R
+++ Kaggle Data/WC Qatar 2022/scraper.R
@@ -3,45 +3,44 @@ setwd("/cloud/project/Kaggle Data/WC Qatar 2022")
 rm(list=ls())
 
 library(dplyr)
-library(tidyr)
 library(rvest)
-library(RSelenium)
+library(jsonlite)
+library(readxl)
+library(RSelenium)  # web scraping
 
-url = "https://www.fifa.com/fifaplus/en/match-centre/match/17/255711/285063/400128082?country=ID&wtw-filter=ALL"
+rm(list=ls())
+
+options(scipen = 99)
+
+# qatar equador
+json_ad = "https://fdh-api.fifa.com/v1/stats/match/128084/teams.json"
+tes = read_json(json_ad)
+nama_var_1 = rep(NA,110)
+nama_var_2 = nama_var_1
+tim_1 = nama_var_1
+tim_2 = nama_var_1
+for(i in 1:110){
+  nama_var_1[i] = tes$`43834`[[i]][1] %>% as.character()
+  tim_1[i] = tes$`43834`[[i]][2] %>% as.numeric()
+  nama_var_2[i] = tes$`43927`[[i]][1] %>% as.character()
+  tim_2[i] = tes$`43927`[[i]][2] %>% as.numeric()
+}
+
+temp = matrix(NA,ncol = 110,nrow = 1) %>% as.data.frame()
+
+temp_1 = temp
+colnames(temp_1) = nama_var_1
+temp_1[1,] = tim_1
+temp_1 = temp_1 %>% mutate(negara = "Qatar")
+
+temp_2 = temp
+colnames(temp_2) = nama_var_2
+temp_2[1,] = tim_2
+temp_2 = temp_2 %>% mutate(negara = "Equador")
+
+final = bind_rows(temp_1,temp_2) %>% janitor::clean_names()
 
 
-# version chrome
-version = "107.0.5304.62"
+final %>% select(goals,negara)
 
-library(wdman)
-
-# start phantomjs instance
-rPJS <- wdman::phantomjs(port = 4680L)
-
-# is it alive?
-rPJS$process$is_alive()
-
-#connect selenium to it?
-rJS <- RSelenium::remoteDriver(browserName="phantomjs", port=4680L)
-
-# open a browser
-rJS$open()
-
-left_team = 
-  url %>% 
-  read_html() %>% 
-  html_nodes(".ff-ml-20") %>% 
-  html_text()
-
-right_team = 
-  url %>% 
-  read_html() %>% 
-  html_nodes(".ff-mr-20") %>% 
-  html_text()
-
-atributes = 
-  url %>% 
-  read_html() %>% 
-  html_nodes(".ff-mt-20 .ff-mb-8") %>% 
-  html_text()
-
+save(final,file = "qatar v equador.rda")
