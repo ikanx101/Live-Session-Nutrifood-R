97b8d475ced70d530e93e2676ab106f831ccdd9c
ikanx101 <ikanx101@gmail.com>
2022-11-30 21:28:24 +0700

    data all

---
+++ Kaggle Data/WC Qatar 2022/rekap all.R
@@ -0,0 +1,20 @@
+rm(list=ls())
+
+setwd("~/Live-Session-Nutrifood-R/Kaggle Data/WC Qatar 2022")
+
+library(dplyr)
+library(tidyr)
+
+list_rda = list.files(pattern = "*rda")
+temp = vector("list",length(list_rda))
+for(i in 1:length(list_rda)){
+  load(list_rda[i])
+  temp[[i]] = final
+  print(i)
+}
+
+final_data = do.call(rbind,temp) %>% mutate(negara = ifelse(negara == "jepang","japan",negara),
+                                            negara = ifelse(negara == "jerman","germany",negara))
+final_data$negara %>% unique() %>% sort()
+
+save(final_data,file = "~/Live-Session-Nutrifood-R/Kaggle Data/WC Qatar 2022/rekapan all/all.rda")
--- Kaggle Data/WC Qatar 2022/scraper.R
+++ Kaggle Data/WC Qatar 2022/scraper.R
@@ -1,4 +1,4 @@
-setwd("/cloud/project/Kaggle Data/WC Qatar 2022")
+setwd("~/Live-Session-Nutrifood-R/Kaggle Data/WC Qatar 2022")
 
 rm(list=ls())
 
@@ -8,39 +8,53 @@ library(jsonlite)
 library(readxl)
 library(RSelenium)  # web scraping
 
+url_utama = "https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022/scores-fixtures?country=ID&wtw-filter=ALL"
+
 rm(list=ls())
 
 options(scipen = 99)
 
-# qatar equador
-json_ad = "https://fdh-api.fifa.com/v1/stats/match/128084/teams.json"
+match = "iran v usa"
+json_ad = "https://fdh-api.fifa.com/v1/stats/match/133028/teams.json"
+
+# ======================================================================
+nama_rda = paste0(match,".rda")
+nama_tim = strsplit(match,split = " ") %>% unlist()
+nama_tim_1 = nama_tim[1]
+nama_tim_2 = nama_tim[3]
+
+
 tes = read_json(json_ad)
 nama_var_1 = rep(NA,110)
 nama_var_2 = nama_var_1
 tim_1 = nama_var_1
 tim_2 = nama_var_1
+
 for(i in 1:110){
-  nama_var_1[i] = tes$`43834`[[i]][1] %>% as.character()
-  tim_1[i] = tes$`43834`[[i]][2] %>% as.numeric()
-  nama_var_2[i] = tes$`43927`[[i]][1] %>% as.character()
-  tim_2[i] = tes$`43927`[[i]][2] %>% as.numeric()
+  nama_var_1[i] = tes[[2]][[i]][1] %>% as.character()
+  tim_1[i] = tes[[2]][[i]][2] %>% as.numeric()
+  
+  nama_var_2[i] = tes[[3]][[i]][1] %>% as.character()
+  tim_2[i] = tes[[3]][[i]][2] %>% as.numeric()
 }
 
 temp = matrix(NA,ncol = 110,nrow = 1) %>% as.data.frame()
 
+# dataframe pertama
 temp_1 = temp
 colnames(temp_1) = nama_var_1
 temp_1[1,] = tim_1
-temp_1 = temp_1 %>% mutate(negara = "Qatar")
-
+temp_1 = temp_1 %>% mutate(negara = nama_tim_1)
+# dataframe kedua
 temp_2 = temp
 colnames(temp_2) = nama_var_2
 temp_2[1,] = tim_2
-temp_2 = temp_2 %>% mutate(negara = "Equador")
+temp_2 = temp_2 %>% mutate(negara = nama_tim_2)
 
 final = bind_rows(temp_1,temp_2) %>% janitor::clean_names()
 
+# final check
+final$status = c("lose","win")
+final %>% select(possession,goals,negara,status)
 
-final %>% select(goals,negara)
-
-save(final,file = "qatar v equador.rda")
+save(final,file = nama_rda)
