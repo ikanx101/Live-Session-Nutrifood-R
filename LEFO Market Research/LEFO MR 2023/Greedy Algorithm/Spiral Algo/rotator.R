441cd981dd9fbe213a15697410d591443e0a8c41
ikanx101 <ikanx101@gmail.com>
2023-03-01 08:43:15 +0000

    save sampai sini dl

---
+++ LEFO Market Research/LEFO MR 2023/Greedy Algorithm/Spiral Algo/rotator.R
@@ -0,0 +1,42 @@
+# ==============================================================================
+# membuat function rotation matrix
+buat_rot_mat = function(theta,n){
+  # buat template sebuah matriks identitas
+  temp_mat = matrix(0,ncol = n,nrow = n)
+  diag(temp_mat) = 1
+  
+  # buat matriks identitas terlebih dahulu
+  mat_rot = temp_mat
+  # membuat isi matriks rotasi
+  for(i in 1:(n-1)){
+    for(j in 1:i){
+      temp = temp_mat
+      idx = n-i
+      idy = n+1-j
+      # print(paste0("Matriks rotasi untuk ",idx," - ",idy,": DONE"))
+      temp[idx,idx] = cos(theta)
+      temp[idx,idy] = -sin(theta)
+      temp[idy,idx] = sin(theta)
+      temp[idy,idy] = cos(theta)
+      # assign(paste0("M",idx,idy),temp)
+      mat_rot = mat_rot %*% temp
+      mat_rot = mat_rot
+    }
+  }
+  # output matriks rotasi
+  return(mat_rot)
+}
+# ==============================================================================
+
+
+# ==============================================================================
+# fungsi untuk rotasi dan kontraksi
+ro_kon = function(list){
+  Xt_2 = list
+  # kita rotasikan dan konstraksikan
+  X2 = mat_rotasi %*% (Xt_2 - center_1)
+  X2 = center_1 + (.6 * X2)
+  X2 = round(X2,0)
+  return(X2)
+}
+# ==============================================================================
