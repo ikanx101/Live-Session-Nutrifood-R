76db3eb22d037cd4718012dcba1c39b7e97fb645
ikanx101 <ikanx101@gmail.com>
2023-03-06 15:29:31 +0700

    done

--- LEFO Market Research/LEFO MR 2023/Greedy Algorithm/Spiral Algo/algoritma.R
+++ LEFO Market Research/LEFO MR 2023/Greedy Algorithm/Spiral Algo/algoritma.R
@@ -1,23 +1,24 @@
 rm(list=ls())
-
+setwd("~/Live-Session-Nutrifood-R/LEFO Market Research/LEFO MR 2023/Greedy Algorithm/Spiral Algo")
 
 # ==============================================================================
 # libraries
 library(dplyr)
 library(tidyr)
 library(parallel)
+library(ggplot2)
 
 # kita ambil dulu function-nya
 source("rotator.R")
 
 # kita set corenya berapa
-numcore = 2
+numcore = 5
 
 # banyaknya calon solusi yang hendak di-generate
-n_calon_solusi = 200
+n_calon_solusi = 20
 
 # berapa banyak SDOA dilakukan
-n_SDOA  = 50
+n_SDOA  = 20
 
 # kita buat rumahnya terlebih dahulu
 
@@ -35,7 +36,7 @@ df
 # ==============================================================================
 # kita buat function untuk generate calon solusi dulu
 generate_solusi = function(dummy){
-  runif(2,min = -100,max = 100)
+  runif(2,min = -50,max = 50)
 }
 
 # kita akan buat juga function untuk menghitung obj function
@@ -45,8 +46,20 @@ obj_func = function(list){
   temp         = df
   temp$x       = list[1]
   temp$y       = list[2]
-  temp$mpg_hit = (temp$x * temp$wt) + temp$y 
+  temp$mpg_hit = (temp$x * temp$wt) + temp$y
   Metrics::mae(temp$mpg,temp$mpg_hit)
+}
+
+# function utk bikin grafik
+bikin_grafik = function(list){
+  x    = rep(0,length(list))
+  y    = rep(0,length(list)) 
+  for(i in 1:length(calon_solusi)){
+    x[i] = calon_solusi[[i]][1]
+    y[i] = calon_solusi[[i]][2]
+  }
+  plot = data.frame(x,y) %>% ggplot(aes(x,y)) + geom_point() + ylim(-50,50) + xlim(-50,50)
+  print(plot)
 }
 # ==============================================================================
 
@@ -68,6 +81,10 @@ mat_rotasi   = buat_rot_mat(2*pi/30,2)
 for(iter in 1:n_SDOA){
   # mencari pusat gravitasi
   center_1   = calon_solusi[[which.min(f_hit)]]
+  
+  # plot
+  bikin_grafik(calon_solusi)
+  Sys.sleep(1)
   
   # kita lakukan rotasi dan kontraksi
   calon_solusi_new = mcmapply(ro_kon,calon_solusi,mc.cores = numcore)
