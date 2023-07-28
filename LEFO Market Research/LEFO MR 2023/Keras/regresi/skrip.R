3c7d2ba7597b47d00258b3a79c435d87c976b74f
ikanx101 <ikanx101@gmail.com>
2023-07-28 10:19:32 +0700

    regressi

---
+++ LEFO Market Research/LEFO MR 2023/Keras/regresi/skrip.R
@@ -0,0 +1,119 @@
+rm(list=ls())
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# ==============================================================================
+# fungsi untuk menggambar f(x)
+gambar_fx = function(f,a,b,delta){
+  temp = data.frame(
+    x = seq(a,b,by = delta)
+  ) %>% 
+    mutate(y = f(x)) %>% 
+    filter(!is.nan(y))
+  subtitle = paste0("Pada selang [",a,",",b,"]")
+  caption = "Digambar dengan R\nikanx101.com"
+  temp %>% 
+    ggplot(aes(x,y)) +
+    geom_line(color = "steelblue") +
+    theme_minimal() +
+    labs(title = "Grafik f(x)",
+         subtitle = subtitle,
+         caption = caption)
+  
+}
+
+# ini f(x)
+f = function(x){sin(x) * cos(x) + sin(5^(-x))}
+
+# ini selang
+a = -2
+b = pi
+delta = 0.001
+
+# ini gambarnya
+gambar_fx(f,a,b,delta)
+
+# kita akan buat regresi dari data ini:
+df = data.frame(x = seq(a,b,by = delta))
+df = df %>% mutate(y = sapply(x,f))
+# ==============================================================================
+
+
+# ==============================================================================
+library(keras)
+library(tensorflow)
+library(caret)
+
+# kita buat dulu targetnya
+target   = df$y
+
+# kita buat fungsi untuk membuat macem-macem ya
+reformulasi = function(){
+  # kita buat data train nya
+  df_train = df %>% select(x)
+  df_train %>% 
+    mutate(x_0 = x,
+           x_1 = x^2,
+           x_2 = x^3,
+           x_3 = x^4,
+           x_4 = x^5,
+           x_5 = x/2,
+           x_6 = x/3,
+           x_7 = x/4) %>% 
+    select(-x)
+}
+
+x = reformulasi() %>% as.matrix()
+y = target %>% as.matrix()
+
+head(x)
+
+model = keras_model_sequential() %>% 
+  layer_dense(units = 64, activation="relu", input_shape = ncol(x)) %>% 
+  layer_dense(units = 32, activation = "relu") %>% 
+  layer_dense(units = 16, activation = "relu") %>% 
+  layer_dense(units = 8, activation = "relu") %>% 
+  layer_dense(units = 1, activation="linear")
+
+model %>% compile(
+  loss = "mse",
+  optimizer =  "adam", 
+  metrics = list("mean_absolute_error")
+)
+
+model %>% summary()
+
+model %>% keras::fit(x, y, epochs = 100,verbose = 0)
+scores = model %>% evaluate(x, y, verbose = 0)
+print(scores)
+
+y_pred = model %>% predict(x)
+df$y_pred = y_pred
+
+
+df %>% 
+  ggplot() +
+  geom_line(aes(x,y),color = "blue",linewidth = .5) +
+  geom_line(aes(x,y_pred),color = "red",linewidth = .5,alpha = .5)
+
+
+
+
+# ===================================
+a = -3
+b = 0
+df = data.frame(x = seq(a,b,by = delta))
+df = df %>% mutate(y = sapply(x,f))
+target   = df$y
+x = reformulasi() %>% as.matrix()
+y = target %>% as.matrix()
+y_pred = model %>% predict(x)
+df$y_pred = y_pred
+
+
+df %>% 
+  ggplot() +
+  geom_line(aes(x,y),color = "blue",linewidth = .5) +
+  geom_line(aes(x,y_pred),color = "red",linewidth = .5,alpha = .5)
