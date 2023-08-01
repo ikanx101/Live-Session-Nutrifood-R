2b03d1337d74a2c73645f34fffb435c978a6b308
ikanx101 <ikanx101@gmail.com>
2023-08-01 12:22:06 +0700

    new

--- LEFO Market Research/LEFO MR 2023/Keras/regresi/skrip.R
+++ LEFO Market Research/LEFO MR 2023/Keras/regresi/skrip.R
@@ -30,7 +30,7 @@ f = function(x){sin(x) * cos(x) + sin(5^(-x))}
 # ini selang
 a = -2
 b = pi
-delta = 0.001
+delta = 0.0001
 
 # ini gambarnya
 gambar_fx(f,a,b,delta)
@@ -40,6 +40,20 @@ df = data.frame(x = seq(a,b,by = delta))
 df = df %>% mutate(y = sapply(x,f))
 # ==============================================================================
 
+
+# ==============================================================================
+# percobaan dengan menggunakan polinom order 25
+poli_model5 = lm(y ~ polym(x,degree = 25), data = df)
+pred_y      = predict(poli_model5,df)
+df_poli     = df %>% mutate(y_pred = pred_y)
+df_poli %>% 
+  ggplot() +
+  geom_line(aes(x,y),
+            group = 1,
+            color = "red") +
+  geom_line(aes(x,y_pred),
+            group = 1,
+            color = "blue")
 
 # ==============================================================================
 library(keras)
@@ -61,7 +75,9 @@ reformulasi = function(){
            x_4 = x^5,
            x_5 = x/2,
            x_6 = x/3,
-           x_7 = x/4) %>% 
+           x_7 = x/4,
+           x_8 = x/5,
+           x_9 = exp(x)) %>% 
     select(-x)
 }
 
@@ -74,7 +90,7 @@ model = keras_model_sequential() %>%
   layer_dense(units = 86, activation="relu", input_shape = ncol(x)) %>% 
   layer_dense(units = 21, activation = "relu") %>% 
   layer_dense(units = 12, activation = "relu") %>% 
-  layer_dense(units = 6,  activation = "relu") %>% 
+  layer_dense(units = 7,  activation = "relu") %>% 
   layer_dense(units = 1,  activation = "linear")
 
 model %>% compile(
@@ -88,6 +104,9 @@ model %>% summary()
 model %>% keras::fit(x, y, epochs = 100,verbose = 0)
 scores = model %>% evaluate(x, y, verbose = 0)
 print(scores)
+
+# kita save modelnya terlebih dahulu
+save_model_hdf5(model,"model regresi keras.h5")
 
 y_pred = model %>% predict(x)
 df$y_pred = y_pred
@@ -103,18 +122,25 @@ df %>%
 
 # ===================================
 # preskidi
-a = -3
-b = 0
-df = data.frame(x = seq(a,b,by = delta))
-df = df %>% mutate(y = sapply(x,f))
-target   = df$y
-x = reformulasi() %>% as.matrix()
-y = target %>% as.matrix()
-y_pred = model %>% predict(x)
-df$y_pred = y_pred
+a          = -pi
+b          = 1
+df         = data.frame(x = seq(a,b,by = delta))
+df         = df %>% mutate(y = sapply(x,f))
+# menggunakan model regresi
+y_pred     = poli_model5 %>% predict(df)
+df$y_pred2 = y_pred
+# target
+target     = df$y
+x          = reformulasi() %>% as.matrix()
+y          = target %>% as.matrix()
+# menggunakan model keras
+y_pred     = model %>% predict(x)
+df$y_pred1 = y_pred
 
 
 df %>% 
   ggplot() +
   geom_line(aes(x,y),color = "blue",linewidth = .5) +
-  geom_line(aes(x,y_pred),color = "red",linewidth = .5,alpha = .5)
+  geom_line(aes(x,y_pred1),color = "red",linewidth = .5,alpha = .5) +
+  geom_line(aes(x,y_pred2),color = "green",linewidth = .5,alpha = .5)
+  
